400d6abbf13082c286f51ea6aff4e8862f0e3122
ddozza54 <bananamayong@gmail.com>
2024-04-10 02:09:04 +0900

    7.4 Relationships [#2]

--- lib/db.ts
+++ lib/db.ts
@@ -5,14 +5,12 @@ const db = new PrismaClient();
 // DB에 데이터를 생성하고 만들며 소통하는 방법
 //create 외에 다양한 함수들을 제공한다
 async function test() {
-  const user = await db.user.create({
-    data: {
-      username: 'test',
-      email: 'text.text.com',
-      password: '1234',
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
     },
   });
-  console.log(user);
+  console.log(token);
 }
 test();
 export default db;
