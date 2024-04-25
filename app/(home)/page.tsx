8b25efba1964c1a554f9a9ed76ea841c52fac5f2
ddozza54 <bananamayong@gmail.com>
2024-04-25 20:24:28 +0900

    4.1 create Account Screen [#1]

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     return (
         <div className='flex flex-col items-center justify-between min-h-screen p-6'>
             <div className='my-auto flex flex-col justify-center items-center'>
-                <span className='text-3xl'>🥕</span>
+                <span className='text-9xl'>🥕</span>
                 <h1 className='text-3xl'>당근</h1>
                 <h2>당근 마켓에 어서오세요!</h2>
             </div>
             <div className='flex flex-col w-full items-center'>
                 <Link href='/create-account'
-                    className='w-full my-5 text-center bg-orange-400 p-2 rounded-md text-orange-200'>시작하기</Link>
+                    className='primary-btn py-2.5 text-lg'>시작하기</Link>
                 <div>
                     <span>이미 계정이 있나요?</span>
                     <Link href='/login' className='hover:underline'>로그인</Link>
