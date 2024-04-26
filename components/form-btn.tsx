a050c47710bd8240412dfc4871cb1d5ceab48cf2
ddozza54 <bananamayong@gmail.com>
2024-04-26 16:31:19 +0900

    4.3 Login Screen [#1]

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -3,5 +3,5 @@ interface FormButtonProps {
     text: string;
 }
 export default function FormButton({ loading, text }: FormButtonProps) {
-    return <button disabled={loading} className='primary-btn h-10 disabled:cursor-not-allowed disabled:text-neutral-300 disabled:bg-neutral-400'>{loading ? '로딩 중...' : text}</button>
+    return <button disabled={loading} className='primary-btn  h-10 disabled:cursor-not-allowed disabled:text-neutral-300 disabled:bg-neutral-400'>{loading ? '로딩 중...' : text}</button>
 }
