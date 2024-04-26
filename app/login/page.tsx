f23716c24ff76af0bb935fb764932a1d125ab984
ddozza54 <bananamayong@gmail.com>
2024-04-26 21:54:00 +0900

    5.1 useformStatus [#3]

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,21 @@ import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 
 export default function Login() {
+    async function handleForm(formData: FormData) {
+        "use server";
+        console.log(formData.get('email'), formData.get('password'))
+        console.log('i run in the server!')
+    }
+
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Login with email and password</h2>
             </div>
-            <form className="flex flex-col gap-3">
-                <FormInput type='email' placeholder='Email' required={true} errors={[]} />
-                <FormInput type='password' placeholder='Password' required={true} errors={[]} />
+            <form action={handleForm} className="flex flex-col gap-3">
+                <FormInput name='email' type='email' placeholder='Email' required={true} errors={[]} />
+                <FormInput name='password' type='password' placeholder='Password' required={true} errors={[]} />
                 <FormButton loading={false} text='Create Account' />
             </form>
             <SocialLogin />
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -1,11 +1,16 @@
 interface FormInputProps {
-    type: string; placeholder: string; required: boolean; errors: string[];
+    name: string;
+    type: string;
+    placeholder: string;
+    required: boolean;
+    errors: string[];
 }
 
-export default function FormInput({ type, placeholder, required, errors }: FormInputProps) {
+export default function FormInput({ name, type, placeholder, required, errors }: FormInputProps) {
     return (
         <div className="flex flex-col gap-2">
             <input
+                name={name}
                 className="p-2 bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 transition border-none placeholder:text-neutral-400"
                 type={type}
                 placeholder={placeholder}
